16810f595c4bdd2c4bb7a7a988929903ab16b48c
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-07 17:33:48 +0200

    add test for HomeController

---
+++ src/test/java/com/greenfox/tribesoflagopus/backend/controller/HomeControllerTest.java
@@ -0,0 +1,69 @@
+package com.greenfox.tribesoflagopus.backend.controller;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.junit.Assert.*;
+
+import com.greenfox.tribesoflagopus.backend.BackendApplication;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BackendApplication.class)
+@WebAppConfiguration
+public class HomeControllerTest {
+
+  private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+          MediaType.APPLICATION_JSON.getSubtype(),
+          Charset.forName("utf8"));
+  private MediaType contentTypePlainText = new MediaType(MediaType.TEXT_PLAIN.getType(),
+          MediaType.TEXT_PLAIN.getSubtype(),
+          Charset.forName("utf8"));
+
+  private MockMvc mockMvc;
+  private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+  @Autowired
+  private WebApplicationContext webApplicationContext;
+
+  @Autowired
+  void setConverters(HttpMessageConverter<?>[] converters) {
+
+    this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+            .findAny()
+            .orElse(null);
+
+    assertNotNull("the JSON message converter must not be null",
+            this.mappingJackson2HttpMessageConverter);
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    this.mockMvc = webAppContextSetup(webApplicationContext).build();
+  }
+
+  @Test
+  public void greeting() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(contentTypePlainText))
+            .andExpect(content().string("hello"));
+  }
+
+}
