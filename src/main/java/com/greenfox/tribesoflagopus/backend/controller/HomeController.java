58a52b41b7c4f770a1fcf02087eb33837751d218
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-07 17:23:12 +0200

    add HomeController

---
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.greenfox.tribesoflagopus.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @GetMapping("/")
+  public String greeting() {
+    return "hello";
+  }
+
+}
