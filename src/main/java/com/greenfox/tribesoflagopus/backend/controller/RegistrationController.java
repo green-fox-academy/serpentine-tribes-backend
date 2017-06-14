c08a6b72428a669eb41c4f3c327866027da22a08
nokecskes <nokecskes@gmail.com>
2017-06-14 17:29:05 +0200

    change registerUser method to return responseEntitiy, add error status in case of occupied username, add badrequest and ok httpstatus

--- src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import com.greenfox.tribesoflagopus.backend.model.dto.StatusResponse;
 import com.greenfox.tribesoflagopus.backend.model.dto.UserRegisterInput;
 import com.greenfox.tribesoflagopus.backend.model.entity.Player;
 import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,30 +14,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
 
   @PostMapping(value = "/register")
-  public JsonDto registerUser(
-          @Valid UserRegisterInput registerInput,
-          BindingResult bindingResult) {
+  public ResponseEntity<JsonDto> registerUser(
+      @Valid UserRegisterInput registerInput,
+      BindingResult bindingResult) {
 
     if (registerInput.getKingdom() == null
-            || registerInput.getKingdom().equals("")) {
-
+        || registerInput.getKingdom().equals("")) {
       registerInput.setKingdom(
-              String.format("%s's kingdom",
-                      registerInput.getUsername()));
+          String.format("%s's kingdom",
+              registerInput.getUsername()));
     }
 
     if (bindingResult.hasErrors()) {
-      return StatusResponse.builder()
-              .status("error")
-              .message("Missing parameter(s):")
-              .build();
+      StatusResponse missingParameterStatus = StatusResponse.builder()
+          .status("error")
+          .message("Missing parameter(s):")
+          .build();
+      return ResponseEntity.badRequest().body(missingParameterStatus);
     }
 
-    return Player.builder()
-            .id(1L)
-            .username("Bond")
-            .kingdomId(1L)
-            .build();
+    if (registerInput.getUsername().equals("occupiedUserName")) {
+      StatusResponse occupiedUserNameStatus = StatusResponse.builder()
+          .status("error")
+          .message("Username already taken, please choose an other one.")
+          .build();
+      return ResponseEntity.status(409).body(occupiedUserNameStatus);
+    }
+
+    Player mockPlayer = Player.builder()
+        .id(1L)
+        .username("Bond")
+        .kingdomId(1L)
+        .build();
+    return ResponseEntity.ok().body(mockPlayer);
   }
 
 }
