b2402db1eefba841538b17b706536b98b42484ab
nokecskes <nokecskes@gmail.com>
2017-06-14 17:42:52 +0200

    fix player id annotation

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Player.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Player.java
@@ -3,6 +3,9 @@ package com.greenfox.tribesoflagopus.backend.model.entity;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.greenfox.tribesoflagopus.backend.model.dto.JsonDto;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +18,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Player implements JsonDto {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String username;
   private long kingdomId;
