03c3c832eb2944e087dca9dacc46af902141c414
nokecskes <nokecskes@gmail.com>
2017-06-15 10:04:06 +0200

    registration controller: add raw version of error status response to be able to list all the missing parameters

--- src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
@@ -4,9 +4,11 @@ import com.greenfox.tribesoflagopus.backend.model.dto.JsonDto;
 import com.greenfox.tribesoflagopus.backend.model.dto.StatusResponse;
 import com.greenfox.tribesoflagopus.backend.model.dto.UserRegisterInput;
 import com.greenfox.tribesoflagopus.backend.model.entity.Player;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,19 +20,28 @@ public class RegistrationController {
       @Valid UserRegisterInput registerInput,
       BindingResult bindingResult) {
 
-    /*
     if (registerInput.getKingdom() == null
         || registerInput.getKingdom().equals("")) {
       registerInput.setKingdom(
           String.format("%s's kingdom",
               registerInput.getUsername()));
-    } */
+    }
 
     //TODO: set error message based on missing input field
     if (bindingResult.hasErrors()) {
+
+      List<FieldError> missingFields = bindingResult.getFieldErrors();
+      String statusMessage = "";
+      for (int i = 0; i < missingFields.size(); i++) {
+        if(i != 0) {
+          statusMessage += ", ";
+        }
+        statusMessage += missingFields.get(i).getField();
+      }
+
       StatusResponse missingParameterStatus = StatusResponse.builder()
           .status("error")
-          .message("Missing parameter(s): username!")
+          .message("Missing parameter(s): " + statusMessage + "!")
           .build();
       return ResponseEntity.badRequest().body(missingParameterStatus);
     }
