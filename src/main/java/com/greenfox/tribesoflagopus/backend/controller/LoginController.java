f4ce3d4bf54a3b1667a3411bd1315af49ba1190a
Kristof Majoros <akmajoros1@gmail.com>
2017-06-15 10:51:27 +0200

    all error handling stuff working as they should be

--- src/main/java/com/greenfox/tribesoflagopus/backend/controller/LoginController.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/LoginController.java
@@ -5,11 +5,14 @@ import com.greenfox.tribesoflagopus.backend.model.dto.StatusResponse;
 import com.greenfox.tribesoflagopus.backend.model.dto.UserLoginInput;
 import com.greenfox.tribesoflagopus.backend.model.entity.Player;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class LoginController {
@@ -17,17 +20,28 @@ public class LoginController {
   @PostMapping("/login")
   public JsonDto loginUser(@Valid @RequestBody UserLoginInput loginInput, BindingResult bindingResult){
 
+    List<String> listOfMissingFields = new ArrayList<>();
+    String errors = "";
+
+    for (FieldError fielderror : bindingResult.getFieldErrors()) {
+      listOfMissingFields.add(fielderror.getField());
+    }
+
+    for (int i = 0; i < listOfMissingFields.size(); i++) {
+        errors += listOfMissingFields.get(i) + ", ";
+    }
+
     if (bindingResult.hasErrors()) {
       return StatusResponse.builder()
               .status("error")
-              .message("Missing username or password")
+              .message("Missing parameters: " + errors)
               .build();
     }
 
     if (!loginInput.getUsername().equals("Bond")){
       return StatusResponse.builder()
               .status("error")
-              .message("No such user" + loginInput.getUsername())
+              .message("No such user: " + loginInput.getUsername())
               .build();
     }
 
