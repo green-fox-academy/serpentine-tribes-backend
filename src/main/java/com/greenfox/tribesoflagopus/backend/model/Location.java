75476cde8a9370400947c5858c0622eb1f32fe63
nokecskes <nokecskes@gmail.com>
2017-06-16 11:42:58 +0200

    add entity annotation

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/Location.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/Location.java
@@ -1,5 +1,6 @@
 package com.greenfox.tribesoflagopus.backend.model;
 
+import javax.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
  * Created by K on 2017.06.16..
  */
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
