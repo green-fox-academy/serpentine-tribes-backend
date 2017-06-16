2aff49c4cb1ed951f0891a40ec5c596b4e4a38eb
nokecskes <nokecskes@gmail.com>
2017-06-16 11:19:46 +0200

    add game object location model

---
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/Location.java
@@ -0,0 +1,19 @@
+package com.greenfox.tribesoflagopus.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by K on 2017.06.16..
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+
+  private int x;
+  private int y;
+
+}
