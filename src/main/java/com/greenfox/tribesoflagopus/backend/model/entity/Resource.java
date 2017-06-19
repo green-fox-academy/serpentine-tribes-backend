ad3212efcc913ccc29dbafe80b0184f1986d1c13
nokecskes <nokecskes@gmail.com>
2017-06-19 11:05:49 +0200

    fix resource class: add id field

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Resource.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Resource.java
@@ -1,6 +1,9 @@
 package com.greenfox.tribesoflagopus.backend.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +20,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Resource {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
   private String type;
   private int amount;
   private int generation;
