a8d3ff1e2645581cdde2e436d016cd5996dc44cf
nokecskes <nokecskes@gmail.com>
2017-06-19 11:12:59 +0200

    fic location class:add id field

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Location.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Location.java
@@ -1,6 +1,9 @@
 package com.greenfox.tribesoflagopus.backend.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +18,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Location {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
   private int x;
   private int y;
 
