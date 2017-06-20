254dfc7e57bc95a1366281cf43003d23aaf564d5
nokecskes <nokecskes@gmail.com>
2017-06-20 14:14:13 +0200

    set one to many relation btw kingdom and resources

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Kingdom.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Kingdom.java
@@ -1,9 +1,14 @@
 package com.greenfox.tribesoflagopus.backend.model.entity;
 
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,4 +28,16 @@ public class Kingdom {
   private String name;
   private Long userId;
 
+  @OneToMany(mappedBy = "kingdom", cascade = CascadeType.ALL)
+  List<Resource> resources;
+
+  public void addResources(Resource resource) {
+    resources.add(resource);
+    resource.setKingdom(this);
+  }
+
+  public void removeResources(Resource resource) {
+    resource.setKingdom(null);
+    this.resources.remove(resource);
+  }
 }
