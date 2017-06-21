f698a10966e62049bec8ecf070daaff05ec32f79
nokecskes <nokecskes@gmail.com>
2017-06-21 19:48:33 +0200

    add registration service class and copy registration controller's code there, in service class: create setkingdom method, concatenate string in simpler way, in test class: delete unused import

--- src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationController.java
@@ -1,73 +1,26 @@
 package com.greenfox.tribesoflagopus.backend.controller;
 
 import com.greenfox.tribesoflagopus.backend.model.dto.JsonDto;
-import com.greenfox.tribesoflagopus.backend.model.dto.StatusResponse;
 import com.greenfox.tribesoflagopus.backend.model.dto.UserRegisterInput;
-import com.greenfox.tribesoflagopus.backend.model.entity.Player;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import com.greenfox.tribesoflagopus.backend.service.RegistrationService;
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
 
+  @Autowired
+  RegistrationService registrationService;
+
   @PostMapping(value = "/register")
   public ResponseEntity<JsonDto> registerUser(
           @Valid UserRegisterInput registerInput,
           BindingResult bindingResult) {
-
-    if (registerInput.getKingdom() == null
-            || registerInput.getKingdom().equals("")) {
-      registerInput.setKingdom(
-              String.format("%s's kingdom",
-                      registerInput.getUsername()));
-    }
-
-    if (bindingResult.hasErrors()) {
-
-      List<FieldError> missingFields = bindingResult.getFieldErrors();
-      ArrayList<String> missingFieldNames = new ArrayList<>();
-
-      for (FieldError fieldError : missingFields) {
-        missingFieldNames.add(fieldError.getField());
-      }
-      Collections.sort(missingFieldNames);
-
-      String statusMessage = "";
-      for (int i = 0; i < missingFields.size(); i++) {
-        if(i > 0) {
-          statusMessage += ", ";
-        }
-        statusMessage += missingFieldNames.get(i);
-      }
-
-      StatusResponse missingParameterStatus = StatusResponse.builder()
-              .status("error")
-              .message("Missing parameter(s): " + statusMessage + "!")
-              .build();
-      return ResponseEntity.badRequest().body(missingParameterStatus);
-    }
-
-    if (registerInput.getUsername().equals("occupiedUserName")) {
-      StatusResponse occupiedUserNameStatus = StatusResponse.builder()
-              .status("error")
-              .message("Username already taken, please choose an other one.")
-              .build();
-      return ResponseEntity.status(409).body(occupiedUserNameStatus);
-    }
-
-    Player mockPlayer = Player.builder()
-            .id(1L)
-            .username("Bond")
-            .kingdomId(1L)
-            .build();
-    return ResponseEntity.ok().body(mockPlayer);
+    return registrationService.register(registerInput, bindingResult);
   }
 
 }
--- src/test/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationControllerTest.java
+++ src/test/java/com/greenfox/tribesoflagopus/backend/controller/RegistrationControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.greenfox.tribesoflagopus.backend.BackendApplication;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
