ea5b683345ff61460364feef2c11eae039558c05
Kristof Majoros <akmajoros1@gmail.com>
2017-06-21 14:37:56 +0200

    allargsconstructor deleted from kingdom class

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Kingdom.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Kingdom.java
@@ -4,15 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import lombok.*;
 
 @Entity
 @Builder
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
 public class Kingdom {
 
