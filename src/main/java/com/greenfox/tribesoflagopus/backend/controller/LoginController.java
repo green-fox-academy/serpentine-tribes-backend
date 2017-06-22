abfe6688647a91f9c54fe88977bfcbaf61a3adcf
Kristof Majoros <akmajoros1@gmail.com>
2017-06-22 09:49:15 +0200

    loginservice created, logincontroller refactored

--- src/main/java/com/greenfox/tribesoflagopus/backend/controller/LoginController.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/controller/LoginController.java
@@ -1,63 +1,28 @@
 package com.greenfox.tribesoflagopus.backend.controller;
 
 import com.greenfox.tribesoflagopus.backend.model.dto.JsonDto;
-import com.greenfox.tribesoflagopus.backend.model.dto.StatusResponse;
 import com.greenfox.tribesoflagopus.backend.model.dto.UserLoginInput;
-import com.greenfox.tribesoflagopus.backend.model.entity.Player;
+import com.greenfox.tribesoflagopus.backend.service.LoginService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
 
 @RestController
 public class LoginController {
 
+  @Autowired
+  LoginService loginService;
+
   @PostMapping("/login")
-  public JsonDto loginUser(@Valid UserLoginInput loginInput, BindingResult bindingResult){
-
-    List<String> listOfMissingFields = new ArrayList<>();
-    String errors = "";
-
-    for (FieldError fielderror : bindingResult.getFieldErrors()) {
-      listOfMissingFields.add(fielderror.getField());
-    }
-    Collections.sort(listOfMissingFields);
-
-    for (int i = 0; i < listOfMissingFields.size(); i++) {
-        errors += listOfMissingFields.get(i) + ", ";
-    }
-
-    if (bindingResult.hasErrors()) {
-      return StatusResponse.builder()
-              .status("error")
-              .message("Missing parameter(s): " + errors)
-              .build();
-    }
-
-    if (!loginInput.getUsername().equals("Bond")){
-      return StatusResponse.builder()
-              .status("error")
-              .message("No such user: " + loginInput.getUsername())
-              .build();
-    }
-
-    if (!loginInput.getPassword().equals("password123")){
-      return StatusResponse.builder()
-              .status("error")
-              .message("Wrong password")
-              .build();
-    }
-
-    return Player.builder()
-            .id(1L)
-            .username("Bond")
-            .kingdomId(1L)
-            .build();
+  public ResponseEntity<JsonDto> loginUser(
+          @Valid UserLoginInput userLoginInput,
+          BindingResult bindingResult) {
+    return loginService.login(userLoginInput, bindingResult);
   }
 }
