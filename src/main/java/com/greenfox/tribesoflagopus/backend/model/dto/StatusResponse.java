97555608e9bb36d669f82383dc55b2d25c3d59bb
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-22 14:32:45 +0200

    update StatusResponse

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/dto/StatusResponse.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/dto/StatusResponse.java
@@ -1,6 +1,10 @@
 package com.greenfox.tribesoflagopus.backend.model.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
