0a02f18cbc6e1ca46f714248ff5bee2d9faa9f4f
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-22 14:25:29 +0200

    update Player

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Player.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Player.java
@@ -1,10 +1,16 @@
 package com.greenfox.tribesoflagopus.backend.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,7 +25,12 @@ public class Player {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
   private String username;
-  private long kingdomId;
+
+  @OneToOne(mappedBy = "player", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+  @JsonIdentityReference(alwaysAsId = true)
+  @JsonProperty(value = "kingdomId")
+  private Kingdom kingdom;
 
   @JsonIgnore
   private String password;
