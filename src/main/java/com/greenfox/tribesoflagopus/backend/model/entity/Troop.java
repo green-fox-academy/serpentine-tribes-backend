f7fc8a4b514e50fba0f5871bd821cd42483a16bc
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-22 14:34:09 +0200

    update Troop

--- src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Troop.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/model/entity/Troop.java
@@ -1,35 +1,41 @@
 package com.greenfox.tribesoflagopus.backend.model.entity;
 
-import lombok.*;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
+import javax.persistence.ManyToOne;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Troop {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
+  @JsonIgnore
+  @ManyToOne(fetch = FetchType.EAGER)
+  private Kingdom kingdom;
+
   private int level;
   private int hp;
   private int attack;
   private int defence;
 
-  public Troop(){
-  }
-
   @Builder
-  public Troop(int level, int hp, int attack, int defence){
-    this.level=1;
-    this.hp=hp;
-    this.attack=attack;
-    this.defence=defence;
+  public Troop(int level, int hp, int attack, int defence) {
+    this.level = 1;
+    this.hp = hp;
+    this.attack = attack;
+    this.defence = defence;
   }
 }
