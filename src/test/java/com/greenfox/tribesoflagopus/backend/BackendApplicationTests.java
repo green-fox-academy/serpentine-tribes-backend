a4379b8b5dbe0e602c1db1496cd85494d021d0f6
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-06-23 15:22:52 +0200

    revert failing database config changes

--- src/test/java/com/greenfox/tribesoflagopus/backend/BackendApplicationTests.java
+++ src/test/java/com/greenfox/tribesoflagopus/backend/BackendApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = BackendApplication.class)
+@SpringBootTest
 public class BackendApplicationTests {
 
 	@Test
