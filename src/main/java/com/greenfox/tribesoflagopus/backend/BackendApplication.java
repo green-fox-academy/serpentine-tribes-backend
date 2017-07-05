25c65bace7d90cbfeb14fcaab1e40c35fe8fc6ad
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-05 10:46:46 +0200

    Main app: remove entity creation

--- src/main/java/com/greenfox/tribesoflagopus/backend/BackendApplication.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/BackendApplication.java
@@ -1,11 +1,5 @@
 package com.greenfox.tribesoflagopus.backend;
 
-import com.greenfox.tribesoflagopus.backend.model.entity.Kingdom;
-import com.greenfox.tribesoflagopus.backend.model.entity.User;
-import com.greenfox.tribesoflagopus.backend.model.entity.Troop;
-import com.greenfox.tribesoflagopus.backend.repository.UserRepository;
-import com.greenfox.tribesoflagopus.backend.service.TroopService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,40 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication implements CommandLineRunner {
 
-	@Autowired
-	TroopService troopService;
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
-	@Autowired
-	UserRepository userRepository;
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello, World!");
-
-		if (!userRepository.existsByUsername("Noemi")) {
-			User user = User.builder()
-							.username("Noemi")
-							.password("passnoemi")
-							.build();
-
-			Kingdom kingdom = Kingdom.builder()
-							.name("Noemi's Kingdom")
-							.build();
-
-			user.setKingdom(kingdom);
-			kingdom.setUser(user);
-
-			userRepository.save(user);
-		}
-
-		if (!troopService.existsByUserName("Noemi")) {
-			Troop troop = Troop.builder().build();
-			Long userId = userRepository.findByUsername("Noemi").getId();
-			troopService.addTroopToUsersKingdom(troop, userId);
-		}
-	}
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Hello, World!");
+  }
 }
