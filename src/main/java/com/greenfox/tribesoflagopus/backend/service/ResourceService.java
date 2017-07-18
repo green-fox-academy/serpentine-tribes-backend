da7a4003dab96827ffad292ebde431e406a98354
Kristof Majoros <akmajoros1@gmail.com>
2017-07-18 12:49:16 +0200

    added new methods to resource service to increase gold, food and both at the same time

--- src/main/java/com/greenfox/tribesoflagopus/backend/service/ResourceService.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/service/ResourceService.java
@@ -4,6 +4,7 @@ import com.greenfox.tribesoflagopus.backend.model.entity.Building;
 import com.greenfox.tribesoflagopus.backend.model.entity.BuildingType;
 import com.greenfox.tribesoflagopus.backend.model.entity.Kingdom;
 import com.greenfox.tribesoflagopus.backend.model.entity.Resource;
+import com.greenfox.tribesoflagopus.backend.model.entity.ResourceType;
 import com.greenfox.tribesoflagopus.backend.repository.KingdomRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,12 +30,45 @@ public class ResourceService {
 
   public void calculateGenerationAmountForKingdom(Kingdom kingdom) {
     List<Building> buildingsOfKingdom = kingdom.getBuildings();
-    int buildingGenerationAmount;
-    int kingdomGenerationAmount;
 
     for (Building building : buildingsOfKingdom) {
       if (building.getType().equals(BuildingType.MINE)){
+        increaseGoldGeneration(kingdom, building);
+      } else if (building.getType().equals(BuildingType.FARM)){
+        increaseFoodGeneration(kingdom, building)
+      } else {
+        increaseAllResourcesGeneration(kingdom, building)
+      }
+    }
+    kingdomRepository.save(kingdom);
+  }
 
+  public void increaseGoldGeneration(Kingdom kingdom, Building mine){
+    List<Resource> resourcesPerKingdom = kingdom.getResources();
+    for (Resource resource : resourcesPerKingdom) {
+      if (resource.getType().equals(ResourceType.GOLD)) {
+        int increasedMineGeneration = resource.getGeneration() + mine.getLevel()*10;
+        resource.setGeneration(increasedMineGeneration);
+      }
+    }
+  }
+
+  public void increaseFoodGeneration(Kingdom kingdom, Building farm){
+    List<Resource> resourcesPerKingdom = kingdom.getResources();
+    for (Resource resource : resourcesPerKingdom) {
+      if (resource.getType().equals(ResourceType.FOOD)) {
+        int increasedMineGeneration = resource.getGeneration() + farm.getLevel()*10;
+        resource.setGeneration(increasedMineGeneration);
+      }
+    }
+  }
+
+  public void increaseAllResourcesGeneration(Kingdom kingdom, Building farm){
+    List<Resource> resourcesPerKingdom = kingdom.getResources();
+    for (Resource resource : resourcesPerKingdom) {
+      if (resource.getType().equals(ResourceType.FOOD)) {
+        int increasedMineGeneration = resource.getGeneration() + farm.getLevel()*10;
+        resource.setGeneration(increasedMineGeneration);
       }
     }
   }
