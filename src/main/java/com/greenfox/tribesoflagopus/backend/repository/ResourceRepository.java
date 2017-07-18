d642e9a1b346600292cca5807876e8ea98590b8d
Marcell Meszaros <Marcell.Meszaros@zoho.com>
2017-07-18 15:54:56 +0200

    Merge from 'development' (background resource service)

--- src/main/java/com/greenfox/tribesoflagopus/backend/repository/ResourceRepository.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/repository/ResourceRepository.java
@@ -1,6 +1,5 @@
 package com.greenfox.tribesoflagopus.backend.repository;
 
-import com.greenfox.tribesoflagopus.backend.model.entity.Building;
 import com.greenfox.tribesoflagopus.backend.model.entity.Resource;
 import com.greenfox.tribesoflagopus.backend.model.entity.ResourceType;
 import org.springframework.data.repository.CrudRepository;
--- src/main/java/com/greenfox/tribesoflagopus/backend/service/BackgroundTaskService.java
+++ src/main/java/com/greenfox/tribesoflagopus/backend/service/BackgroundTaskService.java
@@ -1,13 +1,11 @@
 package com.greenfox.tribesoflagopus.backend.service;
 
 import com.greenfox.tribesoflagopus.backend.model.entity.Kingdom;
-import com.greenfox.tribesoflagopus.backend.model.entity.Resource;
 import com.greenfox.tribesoflagopus.backend.repository.KingdomRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
